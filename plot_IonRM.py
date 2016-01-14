2ad5f7880390ab8363207f9121c765fe80688342
immanuelw <immwa@sas.upenn.edu>
2016-01-14 15:04:25 -0500

    plot follows pattern but values still inexplicably off

--- plot_IonRM.py
+++ plot_IonRM.py
@@ -1,13 +1,16 @@
 # Ugh - he doesn't even have the decency to provide this
 import numpy as np
 import pylab as plt
+import os
 
 path = '/Users/jaguirre/PyModules/ionFR/'
+#path = os.path.expanduser('~/radionopy/')
 
-UT, TEC, B, RM, dRM = np.loadtxt(path+'IonRM.txt',unpack=True)
+UT, TEC, B, RM, dRM = np.loadtxt(os.path.join(path, 'IonRM.txt'), unpack=True)
 
 plt.clf()
-plt.errorbar(UT[0:23],RM[0:23],yerr=dRM[0:23],marker='o',ls='None')
-plt.ylim([0,3])
-plt.xlim([0,24])
+plt.errorbar(UT[0:24], RM[0:24], yerr=dRM[0:24], marker='o', ls='None')
+plt.ylim([0, 3])
+plt.xlim([0, 25])
+plt.savefig('test.pdf')
 plt.show()
