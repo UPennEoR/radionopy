3fec5c1e7ada1d2ef8e49a016e50adc2779e84e7
Saul A. Kohn <saul.aryeh.kohn@gmail.com>
2016-04-12 09:44:41 -0400

    psa64 dates

--- ion_altaz.py
+++ ion_altaz.py
@@ -121,10 +121,18 @@ if __name__ == '__main__':
     #time_str = '2012-02-13T00:00:00'
 
     time_part = 'T00:00:00'
-    # 7 Dec 2011 to 27 Feb 2012
+    # Moore et al.: 7 Dec 2011 to 27 Feb 2012
+    """
     dates = (('2011-12', range(6, 32)),
              ('2012-01', range(1, 32)),
              ('2012-02', range(1, 29)))
+    """
+    # Kohn et al.: 18 Nov 2012 to 26 Mar 2013
+    dates = (('2012-11', range(18, 31)),
+             ('2012-12', range(1, 32)),
+             ('2013-01', range(1, 32)),
+             ('2013-02', range(1, 29)),
+             ('2013-03', range(1, 32)))
     date_strs = ('-'.join((ym, rad.std_hour(day, verbose=False))) for ym, days in dates for day in days)
     time_strs = (''.join((date_str, time_part)) for date_str in date_strs)
 
