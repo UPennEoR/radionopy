3d82e2abcccd61bf110f2c09b9d94d1c079b50af
immanuelw <immwa@sas.upenn.edu>
2016-04-19 18:06:33 -0400

    does all the setup in one script

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import find_packages
-__version__ = '1.0.1-dev'
+import os
+import subprocess
+
+__version__ = '1.0.0-dev'
 
 setup_args = {
     'name': 'radionopy',
@@ -10,12 +13,11 @@ setup_args = {
     'license': '?',
     'package_dir' : {'radionopy': ''},
     'packages' : find_packages(),
-    #'install_requires': ['alembic>=0.8.2', 'Flask>=0.10.1', 'Flask-Login>=0.2.11', 'Flask-Migrate>=1.5.1', 'Flask-Script>=2.0.5',
-    #                       'Flask-SQLAlchemy>=2.0', 'itsdangerous>=0.24', 'Jinja2>=2.8', 'Mako>=1.0.2', 'MarkupSafe>=0.23',
-    #                       'mysqlclient>=1.3.6', 'psycopg2>=2.6.1', 'python-editor>=0.4', 'requests>=2.7.0', 'requests-futures>=0.9.5',
-    #                       'SQLAlchemy>=1.0.8', 'Werkzeug>=0.10.4', 'wheel>=0.24.0', 'paramiko', 'prettytable', 'numpy'],
     'version': __version__,
 }
+
+base_path = os.path.expanduser(os.getcwd())
+rad_path = os.path.join(base_path, 'radiono')
 
 if __name__ == '__main__':
     try:
@@ -26,3 +28,10 @@ if __name__ == '__main__':
         apply(setup, (), setup_args)
     except:
         setup(**setup_args)
+
+    #Compiles geomag C script
+    script_program = 'gcc'
+    script_data = os.path.join(rad_path, 'IGRF/geomag70_linux/geomag70.c')
+    script_name = os.path.join(rad_path, 'IGRF/geomag70_linux/geomag70')
+    script_option = '-o'
+    subprocess.call([script_program, script_data, script_option, script_name])
