13025ae955e93d3577f2ad705d98c26aff4ce897
Zachary Martinot <zmartinot@gmail.com>
2016-08-18 13:40:09 -0400

    added initialization of text files for IGRF data, deleted unneeded import in __init__

--- radiono/__init__.py
+++ radiono/__init__.py
@@ -14,5 +14,4 @@ root_dir = os.path.abspath(os.path.join(rad_dir, '..'))
 rm_dir = os.path.join(root_dir, 'RM_files')
 ionex_dir = os.path.join(root_dir, 'TEC')
 
-from rm import RM #XXX I want to remove this call
 import utils, ionex_file, interp, physics, rm #<-- I would like this to be the only line of code...
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 import subprocess
 
 __version__ = '1.0.0-dev'
-authors = 'James Aguirre, Immanuel Washington, Saul Kohn, Zachary Martino'
+authors = 'James Aguirre, Immanuel Washington, Saul Kohn, Zachary Martinot'
 
 setup_args = {
     'name': 'radionopy',
@@ -36,3 +36,9 @@ if __name__ == '__main__':
     script_option = '-o'
     script_fix = '-lm'
     subprocess.call([script_program, script_data, script_option, script_name, script_fix])
+
+    open(igrf_dir + '/input.txt', 'a').close()
+    open(igrf_dir + '/output.txt', 'a').close()
+
+    # os.system('touch ' + igrf_dir + '/input.txt')
+    # os.system('touch ' + igrf_dir + '/output.txt')
