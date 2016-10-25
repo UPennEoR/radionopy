184aab5872479f679898c76648e866d52e857bf6
Saul A. Kohn <saul.aryeh.kohn@gmail.com>
2016-10-25 09:25:18 -0400

    orthview RMs, cartview TECs

--- scripts/earthPlot.py
+++ scripts/earthPlot.py
@@ -3,12 +3,17 @@ import numpy as np
 from matplotlib import pyplot as plt
 from radiono import rm
 from mpl_toolkits.basemap import Basemap
+import sys,os
 
 NSIDE=16
+"""
 YYYY='2011'
 MM='04'
 DD='11'
-
+"""
+YYYY='2012'
+MM='02'
+DD='13'
 
 def IndexToDeclRa(index,deg=False):
     theta,phi=hp.pixelfunc.pix2ang(NSIDE,index)
@@ -18,8 +23,11 @@ def DeclRaToIndex(decl,RA):
     return hp.pixelfunc.ang2pix(NSIDE,np.radians(-decl+90.),np.radians(360.-RA))
 
 
-IM = rm.IonoMap('0d00m0.0sn','0d00m00.0se',['%s-%s-%s'%(YYYY,MM,DD)])
+IM = rm.IonoMap('30d43m17.5ss','21d25m41.9se',['%s-%s-%s'%(YYYY,MM,DD)])
 #is this silly?
+Plat=-30.-(43./60.)-(17.5/3600.)
+Plon=21.+(25./60.)+(41.9/3600.)
+
 ipix = range(hp.nside2npix(NSIDE))
 ras,decs = [],[]
 for p in ipix:
@@ -30,24 +38,25 @@ for p in ipix:
 tec,rmstec,ionh = IM.ionex_data(YYYY,MM,DD)
 IM.get_radec_RM(ras,decs)
 
-UT=6
-tecUT=hp.cartview(tec[UT,:],flip='geo',return_projected_map=True)
-rmUT = hp.cartview(IM.RMs[0,UT,:],flip='geo',return_projected_map=True)
-
-m = Basemap(projection='cyl', resolution='c')
-
-plt.figure(0)
-m.drawcoastlines()
-_tec = m.imshow(tecUT, alpha=0.6)
-plt.title('UT %i hr, %s-%s-%s, TEC'%(UT,YYYY,MM,DD))
-m.colorbar(location='bottom')
-plt.show()
-plt.close()
-
-plt.figure(0)
-m.drawcoastlines()
-_rm = m.imshow(rmUT, alpha=0.6)
-plt.title('UT %i hr, %s-%s-%s, RM'%(UT,YYYY,MM,DD))
-m.colorbar(location='bottom')
-plt.show()
-plt.close()
+mc = Basemap(projection='cyl', resolution='c')
+mo = Basemap(projection='ortho',lon_0=Plon,lat_0=Plat,resolution='c')
+ 
+for ut in range(24):
+    print 'UT=%i'%ut
+    
+    tecUT=hp.cartview(0.1*tec[ut,:],flip='geo',max=90,min=0,return_projected_map=True)
+    mc.drawcoastlines()
+    _tec = mc.imshow(tecUT, alpha=0.6,vmax=90,vmin=0)
+    plt.title('UT %i hr, %s-%s-%s, TEC'%(ut,YYYY,MM,DD))
+    mc.colorbar(location='bottom')
+    #plt.show()
+    plt.savefig('TEC_%s-%s-%s_UT%i.png'%(YYYY,MM,DD,ut))
+    plt.close()
+    
+    hp.orthview(IM.RMs[0,ut,:],half_sky=True,rot=[0,90],flip='astro',max=5,min=-2,unit=r'${\rm rad\,m^{-2}}$',return_projected_map=True)
+    #mo.drawcoastlines()
+    #_rm = mo.imshow(rmUT,alpha=0.6,vmax=3,vmin=-3)
+    plt.title('UT %i hr, %s-%s-%s'%(ut,YYYY,MM,DD))
+    #mo.colorbar(location='bottom')
+    plt.savefig('RM_%s-%s-%s_UT%i.png'%(YYYY,MM,DD,ut))
+    plt.close()
